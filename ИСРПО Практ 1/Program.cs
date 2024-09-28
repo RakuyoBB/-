1418099b3143187de03ee74bb0b08a1a844bdbc2
исп-32 <-32@LAB25-01>
2024-09-28 09:29:22 +0300

    121

--- ИСРПО Практ 1/Program.cs
+++ ИСРПО Практ 1/Program.cs
@@ -14,34 +14,36 @@ namespace ИСРПО_Практ_1
             Console.WriteLine("Введите количество строк:");
             if (int.TryParse(Console.ReadLine(), out int n))
             {
-                
+
                 Console.WriteLine("Введите количество столбцов:");
                 if (int.TryParse(Console.ReadLine(), out int m))
                 {
 
-                    double[,] matrix = new double[n, m];
-                    int polozElem = 0; //значение для положительных элементов
+                    double[,] matrix = SozdMatr(n, m);
 
-
-                    for (int i = 0; i < n; i++)
-                    {
-                        for (int j = 0; j < m; j++)
-                        {
-                            matrix[i, j] = Math.Sin(i + (j / 12));
-                            if (matrix[i, j] > 0)
-                            {
-                                polozElem++;
-                            }
-                        }
-                    }
-
-
-                    Console.WriteLine("Количество положительных элементов в матрице:" + polozElem);
                 }
-                else { Console.WriteLine("Неверный ввод");}
+                else { Console.WriteLine("Неверный ввод"); }
             }
-            else {Console.WriteLine("Неверный ввод");}
+            else { Console.WriteLine("Неверный ввод"); }
             Console.ReadKey();
+        }
+        static double[,] SozdMatr(int n, int m)
+        {
+            int polozElem = 0; //значение для положительных элементов
+            double[,] matrix = new double[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = Math.Sin(i + (j / 12));
+                    if (matrix[i, j] > 0)
+                    {
+                        polozElem++;
+                    }
+                }
+            }
+            Console.WriteLine("Количество положительных элементов в матрице:" + polozElem);
+            return matrix;
         }
     }
 }
